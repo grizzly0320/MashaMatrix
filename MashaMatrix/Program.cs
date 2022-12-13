329a1e5cdc2a63135853f9a7c83be9726e753f6f
grizzly0320 <91096782+grizzly0320@users.noreply.github.com>
2022-12-13 21:50:34 +0300

    Добавьте файлы проекта.

---
+++ MashaMatrix/Program.cs
@@ -0,0 +1,9 @@
+﻿using Practice_Matrices;
+using System;
+
+Matr m = new Matr("C:/Users/minda/Desktop/aaa.txt");
+InMatr n = new InMatr(m);
+SQMatr s = new SQMatr(1);
+m.show();
+n.Inverce().show();
+(n.Inverce() * m).show();
